42a807a1cb49ea41df9b0e5302efe2100756710d
7hoenix <jpholzmann@gmail.com>
2020-06-21 20:51:52 -0500

    Add dtls, but it doesn't compile.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, happy world!");
+use std::{error::Error, net::SocketAddrV4};
+use tokio::net::{UdpSocket};
+use tokio_openssl::{connect};
+use openssl::ssl::{SslMethod, SslConnector};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let addr: SocketAddrV4 = "127.0.0.1:19324".parse()?;
+
+    let socket = UdpSocket::bind(addr).await?;
+
+    let ssl_connector = SslConnector::builder(SslMethod::dtls())?.build();
+
+    let configuration = ssl_connector.configure()?;
+
+    connect(configuration, "", socket).await?;
+
+    Ok(())
 }
