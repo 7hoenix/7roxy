4fa6970ccd13bfbea7c7d484f9210df88ed8ce1c
7hoenix <jpholzmann@gmail.com>
2020-07-02 09:49:54 -0500

    Minimal implementation working.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use hyper::{Client, Uri};
+use hyper::{Uri, Client};
+use hyper_tls::HttpsConnector;
 
-// https://stackoverflow.com/a/14189088/3818139
 #[tokio::main]
 async fn main() {
-    let client = Client::new();
+    let https = HttpsConnector::new();
+    let client = Client::builder()
+        .build::<_, hyper::Body>(https);
 
-    let url: Uri = "http://httpbin.org/response-headers?foo=bar"
-        .parse()
-        .unwrap();
-    assert_eq!(url.query(), Some("foo=bar"));
+    let url = Uri::from_static("https://api.duckduckgo.com/?q=apple&format=json&t=7roxy");
+    assert_eq!(url.scheme_str(), Some("https"));
 
     match client.get(url).await {
-        Ok(res) => println!("Response: {}", res.status()),
+        Ok(res) => println!("Response: {:?}", res),
         Err(err) => println!("Error: {}", err),
     }
 }
