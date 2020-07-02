78be1ed6ab171a240ce1b256ed8b75009eaad6ec
7hoenix <jpholzmann@gmail.com>
2020-07-02 08:41:19 -0500

    Add basic request / response using hyper copy / pasta.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, happy world!");
+use hyper::{Client, Uri};
+
+// https://stackoverflow.com/a/14189088/3818139
+#[tokio::main]
+async fn main() {
+    let client = Client::new();
+
+    let url: Uri = "http://httpbin.org/response-headers?foo=bar"
+        .parse()
+        .unwrap();
+    assert_eq!(url.query(), Some("foo=bar"));
+
+    match client.get(url).await {
+        Ok(res) => println!("Response: {}", res.status()),
+        Err(err) => println!("Error: {}", err),
+    }
 }
