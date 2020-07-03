3207b5ec3953f202e313a9f8021d4bbbd40833ee
7hoenix <jpholzmann@gmail.com>
2020-07-03 06:29:02 -0500

    Reset this - Using ddg library.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
+use ddg::Query;
+
 use hyper::{Uri, Client};
 use hyper_tls::HttpsConnector;
 
 #[tokio::main]
 async fn main() {
-    let https = HttpsConnector::new();
-    let client = Client::builder()
-        .build::<_, hyper::Body>(https);
+    const APP_NAME: &'static str = "7roxy";
+    let query = Query::new("apple", APP_NAME);
+    let response = query.execute().unwrap();
+    println!("Response: {:#?}", response);
+    // let https = HttpsConnector::new();
+    // let client = Client::builder()
+    //     .build::<_, hyper::Body>(https);
 
-    let url = Uri::from_static("https://api.duckduckgo.com/?q=apple&format=json&t=7roxy");
-    assert_eq!(url.scheme_str(), Some("https"));
+    // let url = Uri::from_static("https://api.duckduckgo.com/?q=apple&format=json&t=7roxy");
+    // assert_eq!(url.scheme_str(), Some("https"));
 
-    match client.get(url).await {
-        Ok(res) => println!("Response: {:?}", res),
-        Err(err) => println!("Error: {}", err),
-    }
+    // match client.get(url).await {
+    //     Ok(res) => println!("Response: {:?}", res),
+    //     Err(err) => println!("Error: {}", err),
+    // }
 }
