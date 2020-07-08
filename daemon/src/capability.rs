3274d66edaa291e9a89bbab4f46eb76ac2a0024e
7hoenix <jpholzmann@gmail.com>
2020-07-08 11:50:50 -0500

    Retrieve answers to questions found from search.

--- daemon/src/capability.rs
+++ daemon/src/capability.rs
@@ -1,4 +1,6 @@
 pub mod http {
+    use reqwest::header;
+    use serde::Deserialize;
     use std::error::Error;
 
     pub mod stack_exchange {
@@ -15,36 +17,43 @@ pub mod http {
         //
 
         #[derive(Deserialize, Debug)]
-        pub struct Response {
-            backoff: Option<u64>,
-            error_id: Option<u64>,
-            error_message: Option<String>,
-            error_name: Option<String>,
-            has_more: bool,
-            items: Vec<Item>,
-            page: u64,
-            page_size: u64,
-            quota_max: u64,
-            quota_remaining: u64,
-            total: u64,
-            r#type: String,
+        pub struct Response<i> {
+            // backoff: Option<u64>,
+            // error_id: Option<u64>,
+            // error_message: Option<String>,
+            // error_name: Option<String>,
+            // has_more: bool,
+            pub items: Vec<i>,
+            // page: u64,
+            // page_size: u64,
+            // quota_max: u64,
+            // quota_remaining: u64,
+            // total: u64,
+            // r#type: String,
         }
 
         #[derive(Deserialize, Debug)]
         pub struct Item {
-            tags: Vec<String>,
-            owner: Value,
-            is_answered: bool,
-            view_count: u64,
-            closed_date: Value,
-            answer_count: u64,
-            score: u64,
-            last_activity_date: Value,
-            creation_date: Value,
-            question_id: u64,
-            link: String,
-            closed_reason: String,
-            title: String,
+            // tags: Vec<String>,
+            // owner: Value,
+            // title: String,
+            // is_answered: bool,
+            // view_count: u64,
+            // closed_date: Value,
+            // answer_count: u64,
+            // score: u64,
+            // last_activity_date: Value,
+            // creation_date: Value,
+            pub question_id: u64,
+            // link: String,
+            // closed_reason: String,
+            // title: String,
+        }
+
+        #[derive(Deserialize, Debug)]
+        pub struct Answer {
+            pub answer_id: u64,
+            pub body: String,
         }
     }
 
@@ -68,20 +77,27 @@ pub mod http {
 
                 let response = request.send().await?;
 
-                if response.status().is_success() {
-                    println!("success real");
-                } else {
-                    println!("failure");
-                }
-                let payload: Result<stack_exchange::Response, reqwest::Error> =
-                    response.json().await;
+                let payload: Result<stack_exchange::Response<stack_exchange::Item>, reqwest::Error> =
+                  response.json().await;
+
                 match payload {
-                    Ok(r) => println!("response parsed"),
+                    Ok(r) => {
+                        let question_ids: Vec<String> = r.items.iter().map(|q| q.question_id.to_string()).collect();
+                        let ids = question_ids.join(";");
+                        let url: String = format!("https://api.stackexchange.com/2.2/questions/{}/answers", ids);
+                        let params = [
+                            ("filter", "withbody"),
+                            ("site", "stackoverflow"),
+                        ];
+                        let request = client
+                            .get(&url)
+                            .query(&params);
+                        let response = request.send().await?;
+                        let payload: Result<stack_exchange::Response<stack_exchange::Answer>, reqwest::Error> = response.json().await;
+                        println!("got payload, {:#?}", payload);
+                    }
                     Err(e) => println!("parsing err, {}", e),
                 }
-
-                //let payload: stack_exchange::Response<()> =
-                //  response.json::<stack_exchange::Response<()>>().await?;
 
                 Ok(())
             }
